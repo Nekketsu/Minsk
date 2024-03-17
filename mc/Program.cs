0f32707eed16d828a96462e7f6936e3d8c6c0796
Nekketsu <nekketsu@gmail.com>
2024-03-17 12:55:56 +0100

    Minor fix to PrettyPrint alignment

--- mc/Program.cs
+++ mc/Program.cs
@@ -73,7 +73,7 @@ static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
 
     Console.WriteLine();
 
-    indent += isLast ? "   " : "│   ";
+    indent += isLast ? "   " : "│  ";
 
     var lastChild = node.GetChildren().LastOrDefault();
 
