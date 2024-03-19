8335c05ec5a0f603e04d0fea593a51738de3ba5e
Nekketsu <nekketsu@gmail.com>
2024-03-19 13:00:25 +0100

    Expose SyntaxFacts.GetText()

---
+++ Minsk.Tests/CodeAnalysis/Syntax/SyntaxFactsTests.cs
@@ -0,0 +1,32 @@
+using Minsk.CodeAnalysis.Syntax;
+
+namespace Minsk.Tests.CodeAnalysis.Syntax;
+
+public class SyntaxFactsTests
+{
+    [Theory]
+    [MemberData(nameof(GetSyntaxKindData))]
+    public void SyntaxFacts_GetText_RoundTrip(SyntaxKind kind)
+    {
+        var text = SyntaxFacts.GetText(kind);
+        if (text is null)
+        {
+            return;
+        }
+
+        var tokens = SyntaxTree.ParseTokens(text);
+        var token = Assert.Single(tokens);
+        Assert.Equal(kind, token.Kind);
+        Assert.Equal(text, token.Text);
+    }
+
+    public static IEnumerable<object[]> GetSyntaxKindData()
+    {
+        var kinds = Enum.GetValues<SyntaxKind>();
+        foreach (var kind in kinds)
+        {
+            yield return new object[] { kind };
+        }
+    }
+}
+
