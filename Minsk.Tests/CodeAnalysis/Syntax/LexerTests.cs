33568284cc5f30c995bd89941379148136115af4
Nekketsu <nekketsu@gmail.com>
2024-03-20 13:01:50 +0100

    Add test to verify we text all token kinds

--- Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -4,6 +4,24 @@ namespace Minsk.Tests.CodeAnalysis.Syntax;
 
 public class LexerTests
 {
+    [Fact]
+    public void Lexer_Tests_AllTokens()
+    {
+        var fixedTokens = Enum.GetValues<SyntaxKind>()
+                               .Where(k => k.ToString().EndsWith("Keyword") ||
+                                           k.ToString().EndsWith("Token"))
+                               .ToList();
+
+        var testedTokenKinds = GetTokens().Concat(GetSeparators()).Select(t => t.kind);
+
+        var untestedTokenKinds = new SortedSet<SyntaxKind>();
+        untestedTokenKinds.Remove(SyntaxKind.BadToken);
+        untestedTokenKinds.Remove(SyntaxKind.EndOfFileToken);
+        untestedTokenKinds.ExceptWith(testedTokenKinds);
+
+        Assert.Empty(untestedTokenKinds);
+    }
+
     [Theory]
     [MemberData(nameof(GetTokensData))]
     public void Lexer_Lexes_Token(SyntaxKind kind, string text)
