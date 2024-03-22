ce2f1aad95adca75c532d22e1a4df20c376c23fc
Nekketsu <nekketsu@gmail.com>
2024-03-22 19:00:15 +0100

    Add SyntaxNode.Span

--- Minsk/CodeAnalysis/TextSpan.cs
+++ Minsk/CodeAnalysis/TextSpan.cs
@@ -1,3 +1,4 @@
+
 namespace Minsk.CodeAnalysis;
 
 public struct TextSpan
@@ -11,4 +12,10 @@ public struct TextSpan
     public int Start { get; }
     public int Length { get; }
     public int End => Start + Length;
+
+    public static TextSpan FromBounds(int start, int end)
+    {
+        var length = end - start;
+        return new TextSpan(start, length);
+    }
 }
