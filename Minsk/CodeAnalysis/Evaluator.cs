981fa0692d967258525cb1db1fd1ee70f428e0dd
Nekketsu <nekketsu@gmail.com>
2024-04-16 00:12:15 +0200

    Add function for computing random numbers

--- Minsk/CodeAnalysis/Evaluator.cs
+++ Minsk/CodeAnalysis/Evaluator.cs
@@ -7,6 +7,7 @@ internal sealed class Evaluator
 {
     private readonly BoundBlockStatement _root;
     private readonly Dictionary<VariableSymbol, object> _variables;
+    private Random _random;
 
     private object _lastValue;
 
@@ -221,6 +222,17 @@ internal sealed class Evaluator
             var message = (string)EvaluateExpression(node.Arguments[0]);
             Console.WriteLine(message);
             return null;
+        }
+        else if (node.Function == BuiltinFunctions.Rnd)
+        {
+            var max = (int)EvaluateExpression(node.Arguments[0]);
+
+            if (_random is null)
+            {
+                _random = new Random();
+            }
+
+            return _random.Next(max);
         }
         else
         {
